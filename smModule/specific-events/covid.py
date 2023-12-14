f1abcba3ab81b85b934a680a132b6ce276c39f93
Gabriel <gabriel.zamy15@gmail.com>
2023-12-14 14:22:26 +0100

    adding of specific-events and first code of it

---
+++ smModule/specific-events/covid.py
@@ -0,0 +1,77 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+import os
+import sys
+
+
+spark = SparkSession.builder.appName("Covid period").getOrCreate()
+spark.sparkContext.setLogLevel("OFF")
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
+
+
+
+
+
+
+def best_perf():
+
+    folder_path = "../../stock_market_data/nasdaq/csv/"
+    folder_path_2 = "../../stock_market_data/nyse/csv/"
+    list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
+    files = [f for f in list_of_files if (os.path.isfile(os.path.join(folder_path, f)) or os.path.isfile(os.path.join(folder_path_2, f)))]
+    file_count = len(files)
+    
+    best_perf = 0
+    best_perf_stock = ""
+    best_percentage = 0
+    i=0
+
+    for file_name in files:
+        if file_name.endswith(".csv"):
+            if os.path.isfile(os.path.join(folder_path, file_name)):
+                input_file_path = os.path.join(folder_path, file_name)
+            else:
+                input_file_path = os.path.join(folder_path_2, file_name)
+
+            # Read the CSV file into a DataFrame
+            df = spark.read.csv(input_file_path, header=True, inferSchema=True)
+
+            # Convert the 'Date' column to a DateType
+            df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
+
+            # Extract the year from the 'Date' column
+            df = df.withColumn("Date", F.date_format("Date", "yyyy"))
+            
+            df = df.filter(df["Date"] == 2020)
+
+            # Aggregate the open and close values for each month
+            df = df.groupBy("Date").agg(
+                F.first("Open").alias("Open"),
+                F.last("Adjusted Close").alias("Adjusted Close")
+            )
+            
+
+            # Extract relevant values
+            rdd = df.rdd
+            for row in rdd.collect():
+                open = row["Open"]
+                adj_close = row["Adjusted Close"]
+                year = row["Date"]
+                if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
+                    perf_tmp = adj_close - open
+                    percentage_tmp = (perf_tmp / open) * 100
+                    if percentage_tmp > best_percentage:
+                        best_perf = perf_tmp
+                        best_percentage = percentage_tmp
+                        best_perf_stock = file_name.strip(".csv")
+                        best_perf_year = year
+
+            print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
+            i += 1
+
+    print(f"\r\033[KThe stock {best_perf_stock} has the best performance during the covid period: {best_perf} ({best_percentage}%) on {best_perf_year}")
+
+best_perf()
