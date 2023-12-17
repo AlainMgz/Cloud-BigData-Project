82db0b4bdde6f5416e8d091d3b19a1ace4e0d305
Gabriel <gabriel.zamy15@gmail.com>
2023-12-17 20:31:15 +0100

    optimisation code specific-events

--- smModule/specific-events/2008.py
+++ smModule/specific-events/2008.py
@@ -26,54 +26,7 @@ def best_perf():
     best_perf = 0
     best_perf_stock = ""
     best_percentage = 0
-    best_perf_year = ""
-    i=0
 
-    for file_name in files:
-        if file_name.endswith(".csv"):
-            if os.path.isfile(os.path.join(folder_path, file_name)):
-                input_file_path = os.path.join(folder_path, file_name)
-            else:
-                input_file_path = os.path.join(folder_path_2, file_name)
-
-            # Read the CSV file into a DataFrame
-            df = spark.read.csv(input_file_path, header=True, inferSchema=True)
-
-            # Convert the 'Date' column to a DateType
-            df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
-
-            # Extract the year from the 'Date' column
-            df = df.withColumn("Date", F.date_format("Date", "yyyy"))
-            
-            df = df.filter(df["Date"] >= 2008 )
-            df = df.filter(df["Date"] <= 2009 )
-         
-
-            # Extract relevant values
-            if df.count() > 0 :
-                first_open = df.first()["Open"]
-                last_adj_close = df.select("Adjusted Close").tail(1)[0][0]
-                if isinstance(first_open, float) and isinstance(last_adj_close, float) and first_open > 0:
-                    all_time_perf_tmp = last_adj_close - first_open
-                    percentage_tmp = (all_time_perf_tmp / first_open) * 100
-                    if percentage_tmp > best_percentage:
-                        best_perf = all_time_perf_tmp
-                        best_percentage = percentage_tmp
-                        best_perf_stock = file_name.strip(".csv")
-            print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
-            i += 1
-
-    print(f"\r\033[KThe stock {best_perf_stock} has the best performance during the 2008 crisis : {best_perf} ({best_percentage}%)")
-
-def worst_perf():
-
-    
-    folder_path = "stock_market_data/nasdaq/csv/"
-    folder_path_2 = "stock_market_data/nyse/csv/"
-    list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
-    files = [f for f in list_of_files if (os.path.isfile(os.path.join(folder_path, f)) or os.path.isfile(os.path.join(folder_path_2, f)))]
-    file_count = len(files)
-    
     worst_perf = 0
     worst_perf_stock = ""
     worst_percentage = 0
@@ -97,33 +50,33 @@ def worst_perf():
             
             df = df.filter(df["Date"] >= 2008 )
             df = df.filter(df["Date"] <= 2009 )
-
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
-            
+         
 
             # Extract relevant values
-            rdd = df.rdd
-            for row in rdd.collect():
-                open = row["Open"]
-                adj_close = row["Adjusted Close"]
-                year = row["Date"]
-                if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
-                    perf_tmp = adj_close - open
-                    percentage_tmp = (perf_tmp / open) * 100
+            if df.count() > 0:
+                first_open = df.first()["Open"]
+                last_adj_close = df.select("Adjusted Close").tail(1)[0][0]
+                if isinstance(first_open, float) and isinstance(last_adj_close, float) and first_open > 0:
+                    all_time_perf_tmp = last_adj_close - first_open
+                    percentage_tmp = (all_time_perf_tmp / first_open) * 100
+
+                    if percentage_tmp > best_percentage:
+                        best_perf = all_time_perf_tmp
+                        best_percentage = percentage_tmp
+                        best_perf_stock = file_name.strip(".csv")
+
                     if percentage_tmp < worst_percentage:
-                        worst_perf = perf_tmp
+                        worst_perf = all_time_perf_tmp
                         worst_percentage = percentage_tmp
                         worst_perf_stock = file_name.strip(".csv")
-                        worst_perf_year = year
 
+                                        
             print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
             i += 1
 
-    print(f"\r\033[KThe stock {worst_perf_stock} has the worst performance during the 2008 crisis: {worst_perf} ({worst_percentage}%) on {worst_perf_year}")
+    print(f"\r\033[KThe stock {best_perf_stock} has the best performance during the 2008-2009 crisis : {best_perf} ({best_percentage}%)")
+    print(f"\r\033[KThe stock {worst_perf_stock} has the worst performance during the 2008-2009 crisis: {worst_perf} ({worst_percentage}%)")
+
 
 
 def worst_day():
@@ -161,7 +114,7 @@ def worst_day():
 
 def good_worst_day():
 
-    date_event = "2020-03-20"
+    date_event = "2020-03-20" ###depend de worst_day()
     folder_path = "stock_market_data/nasdaq/csv/"
     folder_path_2 = "stock_market_data/nyse/csv/"
     list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
@@ -174,6 +127,9 @@ def good_worst_day():
     best_upgrade = 0
     best_percentage_up = 0
     best_perf_stock2 = ""
+    div = 0
+    percentage_tot = 0
+
     i=0
 
     for file_name in files:
@@ -190,15 +146,7 @@ def good_worst_day():
             df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
             
             df = df.filter(df["Date"] == date_event)
-
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
             
-
-            # Extract relevant values
             rdd = df.rdd
             for row in rdd.collect():
                 open = row["Open"]
@@ -206,8 +154,6 @@ def good_worst_day():
                 year = row["Date"]
                 if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
                     perf_tmp = adj_close - open
-                    upgrade_tmp = adj_close - open
-                    percentage_up_tmp = (perf_tmp / open) * 100
                     percentage_tmp = (perf_tmp / open) * 100
 
                     if percentage_tmp > best_percentage:
@@ -216,76 +162,24 @@ def good_worst_day():
                         best_perf_stock = file_name.strip(".csv")
                         best_perf_year = year
                     
-                    if upgrade_tmp > best_upgrade:
-                        best_upgrade = upgrade_tmp
-                        best_percentage_up = percentage_up_tmp
+                    if perf_tmp > best_upgrade:
+                        best_upgrade = perf_tmp
+                        best_percentage_up = percentage_tmp
                         best_perf_stock2 = file_name.strip(".csv")
                         best_perf_year = year
+                    
+                    percentage_tot += percentage_tmp
+                    div += 1 
                     
 
             print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
             i += 1
 
+    res = percentage_tot/div
+
+    print(f"\r\033[KThe {date_event} was the worst day for the US market (using the Dow Jones index) during the 2008-2009 crisis, the stocks had an average lost of: ({res}%)")
     print(f"\r\033[KThe stock {best_perf_stock} has the best performance on the {best_perf_year} with: {best_perf} ({best_percentage}%)")
     print(f"\r\033[KBut the stock {best_perf_stock2} has the greatest upgrade on the {best_perf_year} with: {best_upgrade} ({best_percentage_up}%)")
 
 
-def average_evol():
-
-    date_event = "2020-03-20"
-    folder_path = "stock_market_data/nasdaq/csv/"
-    folder_path_2 = "stock_market_data/nyse/csv/"
-    list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
-    files = [f for f in list_of_files if (os.path.isfile(os.path.join(folder_path, f)) or os.path.isfile(os.path.join(folder_path_2, f)))]
-    file_count = len(files)
-    
-    div = 0
-    percentage_tot = 0
-    i=0
-
-    for file_name in files:
-        if file_name.endswith(".csv"):
-            if os.path.isfile(os.path.join(folder_path, file_name)):
-                input_file_path = os.path.join(folder_path, file_name)
-            else:
-                input_file_path = os.path.join(folder_path_2, file_name)
-
-            # Read the CSV file into a DataFrame
-            df = spark.read.csv(input_file_path, header=True, inferSchema=True)
-
-            # Convert the 'Date' column to a DateType
-            df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
-            
-            df = df.filter(df["Date"] == date_event)
-
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
-            
-
-            # Extract relevant values
-            rdd = df.rdd
-            for row in rdd.collect():
-                open = row["Open"]
-                adj_close = row["Adjusted Close"]
-
-                if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
-                    perf_tmp = adj_close - open
-                    percentage_tmp = (perf_tmp / open) * 100
-
-                    percentage_tot += percentage_tmp
-                    div += 1                 
-
-            print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
-            i += 1
-    res = percentage_tot/div
-    print(f"\r\033[KThe {date_event} was the worst day for the US market (using the Dow Jones index) during the 2008 crisis, it has an average lost of: ({res}%)")
- 
-best_perf()
-"""worst_perf()
 worst_day()
-good_worst_day()
-average_evol()"""
-
--- smModule/specific-events/covid.py
+++ smModule/specific-events/covid.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-#spark = SparkSession.builder.appName("Covid period").getOrCreate()
+#spark = SparkSession.builder.appName("covid period").getOrCreate()
 spark = SparkSession.builder.config('spark.driver.host', '127.0.0.1').getOrCreate()
 spark.sparkContext.setLogLevel("OFF")
 if os.name == 'nt':
@@ -26,9 +26,12 @@ def best_perf():
     best_perf = 0
     best_perf_stock = ""
     best_percentage = 0
-    best_perf_year = ""
-    i=0
 
+    worst_perf = 0
+    worst_perf_stock = ""
+    worst_percentage = 0
+
+    i=0
     for file_name in files:
         if file_name.endswith(".csv"):
             if os.path.isfile(os.path.join(folder_path, file_name)):
@@ -53,83 +56,34 @@ def best_perf():
                 if isinstance(first_open, float) and isinstance(last_adj_close, float) and first_open > 0:
                     all_time_perf_tmp = last_adj_close - first_open
                     percentage_tmp = (all_time_perf_tmp / first_open) * 100
+
                     if percentage_tmp > best_percentage:
                         best_perf = all_time_perf_tmp
                         best_percentage = percentage_tmp
                         best_perf_stock = file_name.strip(".csv")
-            print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
-            i += 1
 
-    print(f"\r\033[KThe stock {best_perf_stock} has the best performance during the covid crisis : {best_perf} ({best_percentage}%)")
-
-def worst_perf():
-
-    date_event = 2020
-    folder_path = "stock_market_data/nasdaq/csv/"
-    folder_path_2 = "stock_market_data/nyse/csv/"
-    list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
-    files = [f for f in list_of_files if (os.path.isfile(os.path.join(folder_path, f)) or os.path.isfile(os.path.join(folder_path_2, f)))]
-    file_count = len(files)
-    
-    worst_perf = 0
-    worst_perf_stock = ""
-    worst_percentage = 0
-    worst_perf_year = ""
-    i=0
-
-    for file_name in files:
-        if file_name.endswith(".csv"):
-            if os.path.isfile(os.path.join(folder_path, file_name)):
-                input_file_path = os.path.join(folder_path, file_name)
-            else:
-                input_file_path = os.path.join(folder_path_2, file_name)
-
-            # Read the CSV file into a DataFrame
-            df = spark.read.csv(input_file_path, header=True, inferSchema=True)
-
-            # Convert the 'Date' column to a DateType
-            df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
-
-            # Extract the year from the 'Date' column
-            df = df.withColumn("Date", F.date_format("Date", "yyyy"))
-            
-            df = df.filter(df["Date"] == date_event)
-
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
-            
-
-            # Extract relevant values
-            rdd = df.rdd
-            for row in rdd.collect():
-                open = row["Open"]
-                adj_close = row["Adjusted Close"]
-                year = row["Date"]
-                if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
-                    perf_tmp = adj_close - open
-                    percentage_tmp = (perf_tmp / open) * 100
                     if percentage_tmp < worst_percentage:
-                        worst_perf = perf_tmp
+                        worst_perf = all_time_perf_tmp
                         worst_percentage = percentage_tmp
                         worst_perf_stock = file_name.strip(".csv")
-                        worst_perf_year = year
 
+                                        
             print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
             i += 1
 
-    print(f"\r\033[KThe stock {worst_perf_stock} has the worst performance during the covid period: {worst_perf} ({worst_percentage}%) on {worst_perf_year}")
+    print(f"\r\033[KThe stock {best_perf_stock} has the best performance during the covid period : {best_perf} ({best_percentage}%)")
+    print(f"\r\033[KThe stock {worst_perf_stock} has the worst performance during the covid period : {worst_perf} ({worst_percentage}%)")
+
+
 
 
 def worst_day():
 
-    date_event = 2020
+    
     worst_perf = 0
     worst_percentage = 0
     worst_perf_day = ""
-    input_file = "stock_market_data/Dow_Jones.csv"
+    input_file = "stock_market_data/Dow_Jones.csv" ###like this or just the folder before and if csv?
     # Read the CSV file into a DataFrame
     df = spark.read.csv(input_file, header=True, inferSchema=True)
     df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
@@ -137,9 +91,8 @@ def worst_day():
     # Create the year column from the 'Date' column
     df = df.withColumn("Year", F.year("Date"))
 
-    df = df.filter(df["Year"] == date_event)
+    df = df.filter(df["Year"] >= 2020 )
 
-    # Extract relevant values
     rdd = df.rdd
     for row in rdd.collect():
         open = row["Open"]
@@ -157,7 +110,7 @@ def worst_day():
 
 def good_worst_day():
 
-    date_event = "2020-03-20"
+    date_event = "2020-03-20" ###depend de worst_day
     folder_path = "stock_market_data/nasdaq/csv/"
     folder_path_2 = "stock_market_data/nyse/csv/"
     list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
@@ -170,9 +123,10 @@ def good_worst_day():
     best_upgrade = 0
     best_percentage_up = 0
     best_perf_stock2 = ""
-    best_perf_year = ""
-    i=0
+    div = 0
+    percentage_tot = 0
 
+    i=0
     for file_name in files:
         if file_name.endswith(".csv"):
             if os.path.isfile(os.path.join(folder_path, file_name)):
@@ -188,14 +142,6 @@ def good_worst_day():
             
             df = df.filter(df["Date"] == date_event)
 
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
-            
-
-            # Extract relevant values
             rdd = df.rdd
             for row in rdd.collect():
                 open = row["Open"]
@@ -203,8 +149,6 @@ def good_worst_day():
                 year = row["Date"]
                 if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
                     perf_tmp = adj_close - open
-                    upgrade_tmp = adj_close - open
-                    percentage_up_tmp = (perf_tmp / open) * 100
                     percentage_tmp = (perf_tmp / open) * 100
 
                     if percentage_tmp > best_percentage:
@@ -213,86 +157,23 @@ def good_worst_day():
                         best_perf_stock = file_name.strip(".csv")
                         best_perf_year = year
                     
-                    if upgrade_tmp > best_upgrade:
-                        best_upgrade = upgrade_tmp
-                        best_percentage_up = percentage_up_tmp
+                    if perf_tmp > best_upgrade:
+                        best_upgrade = perf_tmp
+                        best_percentage_up = percentage_tmp
                         best_perf_stock2 = file_name.strip(".csv")
                         best_perf_year = year
+                    
+                    percentage_tot += percentage_tmp
+                    div += 1 
                     
 
             print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
             i += 1
 
+    res = percentage_tot/div
+
+    print(f"\r\033[KThe {date_event} was the worst day for the US market (using the Dow Jones index) during the covid period, the stocks had an average lost of: ({res}%)")
     print(f"\r\033[KThe stock {best_perf_stock} has the best performance on the {best_perf_year} with: {best_perf} ({best_percentage}%)")
     print(f"\r\033[KBut the stock {best_perf_stock2} has the greatest upgrade on the {best_perf_year} with: {best_upgrade} ({best_percentage_up}%)")
 
-
-def average_evol():
-
-    date_event = "2020-03-20"
-    folder_path = "stock_market_data/nasdaq/csv/"
-    folder_path_2 = "stock_market_data/nyse/csv/"
-    list_of_files = os.listdir(folder_path) + os.listdir(folder_path_2)
-    files = [f for f in list_of_files if (os.path.isfile(os.path.join(folder_path, f)) or os.path.isfile(os.path.join(folder_path_2, f)))]
-    file_count = len(files)
-    
-    div = 0
-    percentage_tot = 0
-    i=0
-
-    for file_name in files:
-        if file_name.endswith(".csv"):
-            if os.path.isfile(os.path.join(folder_path, file_name)):
-                input_file_path = os.path.join(folder_path, file_name)
-            else:
-                input_file_path = os.path.join(folder_path_2, file_name)
-
-            # Read the CSV file into a DataFrame
-            df = spark.read.csv(input_file_path, header=True, inferSchema=True)
-
-            # Convert the 'Date' column to a DateType
-            df = df.withColumn("Date", F.to_date(F.col("Date"), "dd-MM-yyyy"))
-            
-            df = df.filter(df["Date"] == date_event)
-
-            # Aggregate the open and close values for each month
-            df = df.groupBy("Date").agg(
-                F.first("Open").alias("Open"),
-                F.last("Adjusted Close").alias("Adjusted Close")
-            )
-            
-
-            # Extract relevant values
-            rdd = df.rdd
-            for row in rdd.collect():
-                open = row["Open"]
-                adj_close = row["Adjusted Close"]
-
-                if isinstance(adj_close, float) and isinstance(open, float) and open > 0:
-                    perf_tmp = adj_close - open
-                    percentage_tmp = (perf_tmp / open) * 100
-
-                    percentage_tot += percentage_tmp
-                    div += 1                 
-
-            print(f"\r\033[KAnalysing files [{i}/{file_count}]", flush=True, end='')
-            i += 1
-    res = percentage_tot/div
-    print(f"\r\033[KThe {date_event} was the worst day for the US market (using the Dow Jones index) during the covid period, it has an average lost of: ({res}%)")
- 
-
-best_perf()
-worst_perf()
-worst_day()
 good_worst_day()
-average_evol()
-
-
-
-
-
-
-
-
-
-
